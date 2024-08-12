9fab5e8260c1f052eb0e9111b87caf266277ffa8
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-08-12 18:00:23 +0300

    update InstanceChooserLoginFragment.java

--- mastodon/src/main/java/org/joinmastodon/android/fragments/onboarding/InstanceChooserLoginFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/onboarding/InstanceChooserLoginFragment.java
@@ -79,7 +79,7 @@ public class InstanceChooserLoginFragment extends InstanceCatalogFragment{
 		}
 		if(filteredData.isEmpty()){
 			for(CatalogInstance inst:data){
-				if(inst.normalizedDomain.equals("mastodon.social") || inst.normalizedDomain.equals("mastodon.online")){
+				if(inst.normalizedDomain.equals("najmon.com")){
 					filteredData.add(inst);
 				}
 			}
