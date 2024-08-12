e133c45b194881ee6a94d0d16a6eef1695b2728b
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-08-12 09:31:10 +0300

    update InstanceCatalogFacade.java

--- mastodon/src/main/java/org/joinmastodon/android/fragments/onboarding/InstanceCatalogFacade.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/onboarding/InstanceCatalogFacade.java
@@ -1,9 +1,9 @@
 package org.joinmastodon.android.fragments.onboarding;
 
-public class InstanceCatalogFacade extends InstanceCatalogFragment{
+public class InstanceCatalogFacade{
     
-
+/*
     public void loadInstanceInfo(String _domain, boolean isFromRedirect) {
         loadInstanceInfo(_domain, isFromRedirect, null);
-    }
+    }*/
 }
