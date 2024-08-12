2951817df880fb6b7629cc5c7e3dd6992ba14974
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-08-12 08:54:10 +0300

    update InstanceCatalogFacade.java

---
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/onboarding/InstanceCatalogFacade.java
@@ -0,0 +1,9 @@
+package org.joinmastodon.android.fragments.onboarding;
+
+public class InstanceCatalogFacade {
+    private InstanceCatalogFragment instanceCatalogFragment = new InstanceCatalogFragment();
+
+    public void someMethod() {
+        instanceCatalogFragment.someMethod();
+    }
+}
