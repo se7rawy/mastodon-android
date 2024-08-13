b68e25ae15bdf5ba7109fcc673b4e5f693201385
se7rawy <123745448+se7rawy@users.noreply.github.com>
2024-08-13 10:47:45 +0300

    update InstanceChooserLoginFragment.java

--- mastodon/src/main/java/org/joinmastodon/android/fragments/onboarding/InstanceChooserLoginFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/onboarding/InstanceChooserLoginFragment.java
@@ -56,29 +56,30 @@ public class InstanceChooserLoginFragment extends InstanceCatalogFragment{
 		}
 		
 		
-		getN();
+		
 
 		
 	}
 
-void getN(){
+@Override
+	protected void getN(){
 	String s;
 s="najmon.com";
 				if(s.length()>0){
 					fakeInstance.domain=fakeInstance.normalizedDomain=s;
 					fakeInstance.description=getString(R.string.loading_instance);
-				/*	if(filteredData.size()>0 && filteredData.get(0)==fakeInstance){
+					if(filteredData.size()>0 && filteredData.get(0)==fakeInstance){
 						if(list.findViewHolderForAdapterPosition(1) instanceof InstanceViewHolder ivh){
 							ivh.rebind();
 						}
-					}*/
+					}
 					if(filteredData.isEmpty()){
 						filteredData.add(fakeInstance);
-					//	adapter.notifyItemInserted(0);
+						adapter.notifyItemInserted(0);
 					}
-				//	clearBtn.setVisibility(View.VISIBLE);
+					clearBtn.setVisibility(View.VISIBLE);
 				}else{
-					//clearBtn.setVisibility(View.GONE);
+					clearBtn.setVisibility(View.GONE);
 				}
 			
 		
